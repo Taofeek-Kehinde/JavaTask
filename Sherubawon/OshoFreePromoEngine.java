974c0051a06cc59e3a30b20b9c2348e34881e788
Taofeek-Kehinde <Ktaofeek015@gmail.com>
2026-05-13 08:18:25 +0100

    fixing my test files

--- Sherubawon/OshoFreePromoEngine.java
+++ Sherubawon/OshoFreePromoEngine.java
@@ -1,17 +1,17 @@
 public class OshoFreePromoEngine {
     
     public double calculateFinalPrice(double cartTotal, String promoCode) {
- 
-        if (cartTotal == 10000.00 && promoCode.equals("STARTER10")) {
-            return 9000.00;
+        // STARTER10: N5,000 - N14,999
+        if (cartTotal >= 5000 && cartTotal <= 14999 && promoCode.equals("STARTER10")) {
+            return cartTotal * 0.9;
         }
-
-        if (cartTotal == 18000.00 && promoCode.equals("BIGBOY20")) {
-            return 14400.00;
+        // BIGBOY20: N15,000 - N29,999
+        if (cartTotal >= 15000 && cartTotal <= 29999 && promoCode.equals("BIGBOY20")) {
+            return cartTotal * 0.8;
         }
-
-        if (cartTotal == 30000.00 && promoCode.equals("OSHOFREE35")) {
-            return 19500.00;
+        // OSHOFREE35: N30,000 and above
+        if (cartTotal >= 30000 && promoCode.equals("OSHOFREE35")) {
+            return cartTotal * 0.65;
         }
         return cartTotal;
     }
